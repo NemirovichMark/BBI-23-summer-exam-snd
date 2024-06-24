e308b05b9a1c293c214bbfaf711a011fc9a24403
Margarita-Barsukova <159894883+Margarita-Barsukova@users.noreply.github.com>
2024-06-24 19:16:42 +0300

    Add files via upload
    Марк Анатольевич, здравствуйте! Я повторно отправляла переделанный 3 номер на экзамене, но там файлы перепутались и не то сохранилось.  Только что посмотрела. Я отправила в дискорд и тут тоже

---
+++ Task3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant_1
+{
+    public class Task3
+    {
+        public class Searcher
+        {
+            private string input;
+            private string[] output;
+
+            public string Input
+            {
+                get { return input; }
+            }
+
+            public string[] Output
+            {
+                get { return output; }
+            }
+
+            public Searcher(string text)
+            {
+                input = text;
+                Povtors();
+            }
+
+            private void Povtors()
+            {
+                output = input.Split(' ');
+            }
+
+            public override string ToString()
+            {
+                return string.Join(" ", output);
+            }
+        }
+    }
+}
