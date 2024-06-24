253ed262a8fbf0275c9857ce703db5164b2664a2
Ilya Glazkov <146069271+GlazkovIlya@users.noreply.github.com>
2024-06-24 13:36:42 +0300

    Add files via upload

--- Var1/Variant_1/Task4.cs
+++ Var1/Variant_1/Task4.cs
@@ -3,13 +3,65 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 #region Выберите библиотеку(и) для сериализации
-// using Newtonsoft;
-// using System.Text.Json;
-// using System.Text.Json.Serialization;
+using Newtonsoft;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 #endregion
 namespace Variant_1
 {
     public class Task4
     {
+        interface ICreator
+        {
+            void CreateFolder(string path, object file);
+            void CreateFile(string path, object file);
+        }
+
+        public abstract class AbstractSerializer : ICreator
+        {
+            public abstract void Write<T>(T obj, string path);
+            public abstract T Read<T>(string path);
+
+
+            public void CreateFolder(string path, object file)
+            {
+                if (file is string)
+                {
+                    string fileName = (string)file;
+
+                }
+                else if (file is string[])
+                {
+                    string[] fileNames = (string[])file;
+                }
+            }
+            public void CreateFile(string path, object file)
+            {
+                if (file is string)
+                {
+                    string fileName = (string)file;
+
+                }
+                else if (file is string[])
+                {
+                    string[] fileNames = (string[])file;
+                }
+            }
+
+        }
+
+        public class SearcherSerialize : AbstractSerializer
+        {
+            public override void Write<T>(T obj, string path)
+            {
+
+            }
+
+            public override T Read<T>(string path)
+            {
+                string json = File.ReadAllText(path);
+                return JsonSerializer.Deserialize<T>(json);
+            }
+        }
     }
 }
