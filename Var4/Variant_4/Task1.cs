e550727498cc70e8e0ed982cb62b81ec3c5b3857
VladimirLunin07 <146268008+VladimirLunin07@users.noreply.github.com>
2024-06-24 13:22:05 +0300

    Add files via upload

--- Var4/Variant_4/Task1.cs
+++ Var4/Variant_4/Task1.cs
@@ -9,6 +9,120 @@ namespace Variant_4
 {
     public class Task1
     {
-       
+        public Triangle[] Triangles { get; set; }
+
+        public Task1(Triangle[] triangles)
+        {
+            Triangles = triangles;
+        }
+
+        public override string ToString()
+        {
+            string ans = "";
+            foreach (Triangle triangle in Triangles)
+            {
+                ans += triangle + "\n";
+            }
+
+            return ans;
+        }
+
+        public void Sorting()
+        {
+            if (Triangles == null || Triangles.Length == 0)
+                return;
+            QuickSort(0, Triangles.Length - 1);
+        }
+
+        private void QuickSort(int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(low, high);
+                QuickSort(low, pivotIndex - 1);
+                QuickSort(pivotIndex + 1, high);
+            }
+        }
+
+        private int Partition(int low, int high)
+        {
+            Triangle pivot = Triangles[high];
+            double pivotArea = pivot.Area();
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (Triangles[j].Area() <= pivotArea)
+                {
+                    i++;
+                    Swap(ref Triangles[i], ref Triangles[j]);
+                }
+            }
+
+            Swap(ref Triangles[i + 1], ref Triangles[high]);
+            return i + 1;
+        }
+
+        private void Swap(ref Triangle a, ref Triangle b)
+        {
+            Triangle temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public struct Triangle
+        {
+            public int[] abc { get; private set; } = new int[3];
+            public int A { get; private set; }
+            public int B { get; private set; }
+            public int C { get; private set; }
+
+            public Triangle(int[] abc)
+            {
+                if (abc.Length == 3)
+                {
+                    A = abc[0];
+                    B = abc[1];
+                    C = abc[2];
+                    this.abc = abc;
+                    CheckTriangle();
+                }
+            }
+
+            private void CheckTriangle()
+            {
+                if (A + B <= C || A + C <= B || B + C <= A || A < 0 || B < 0 || C < 0)
+                {
+                    abc = new int[] { 0, 0, 0 };
+                }
+            }
+
+            public double Area()
+            {
+                double p = (abc[0] + abc[1] + abc[2]) / 2.0;
+                return Math.Round(Math.Sqrt(p * (p - abc[0]) * (p - abc[1]) * (p - abc[2])), 2);
+            }
+
+            public string Distinct()
+            {
+                if (A == B && B == C && C == A)
+                {
+                    return "равносторонний";
+                }
+
+                if (A == B || B == C || C == A)
+                {
+                    return "равнобедренный";
+                }
+
+                return "разносторонний";
+            }
+
+            public override string ToString()
+            {
+                return
+                    $"Type = Triangle, subtype = {Distinct()}, a = {abc[0]}, b = {abc[1]}, c = {abc[2]}, with S = {Area()}";
+            }
+        }
     }
 }
--- Var4/Variant_4/Task3.cs
+++ Var4/Variant_4/Task3.cs
@@ -1,15 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Http.Headers;
+using System.Drawing;
+using System.Globalization;
 using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Variant_4
 {
-
     public class Task3
     {
-        
+        public class Uniques
+        {
+            public string Input { get; private set; }
+            public string[] Output { get; private set; }
+
+            public Uniques(string text)
+            {
+                Input = text;
+                if (text != null)
+                {
+                    FindAns();
+                }
+            }
+
+            private void FindAns()
+            {
+                char[] delimiters = new char[] { ' ', ',', '.', '!', '?', ':', ';', '\n', '\r' };
+                string[] strings = Input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                List<string> list = new List<string>();
+                foreach (string s in strings)
+                {
+                    if (CheckRepeats(s) && s.Length > 1)
+                    {
+                        list.Add(s);
+                    }
+                }
+
+                Output = new string[list.Count];
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Output[i] = list[i];
+                }
+            }
+
+            private bool CheckRepeats(string word)
+            {
+                List<char> set = new List<char>();
+                foreach (char c in word)
+                {
+                    if (set.Contains(c))
+                    {
+                        return false;
+                    }
+
+                    set.Add(c);
+                }
+
+                return true;
+            }
+
+            public override string ToString()
+            {
+                string ans = "";
+                foreach (string s in Output)
+                {
+                    ans += s + "\n";
+                }
+
+                return ans;
+            }
+        }
     }
 }
